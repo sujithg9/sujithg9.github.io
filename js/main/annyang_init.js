bac11c277ddaf86622468e01bbdbce0ee36913bf
Sujith Gannamaneni <gannaman@us.ibm.com>
2017-09-25 18:35:41 -0400

    Started integrating Speech recognization (annyang plug-in) into website

---
+++ js/main/annyang_init.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	//check SignIn
+	$(function(){
+    if (annyang) {
+
+         var commands = {
+            'about sujith': aboutMe,
+            'show sujith\'s *area': showFlickr,
+          };
+
+          var aboutMe = function(){
+            alert('About Me !!!')
+          }
+
+          var showAccomplishments = function(area) {
+              alert(area)
+          }
+
+          // OPTIONAL: activate debug mode for detailed logging in the console
+          annyang.debug();
+
+          // Add voice commands to respond to
+          annyang.addCommands(commands);
+
+          // OPTIONAL: Set a language for speech recognition (defaults to English)
+          // For a full list of language codes, see the documentation:
+          // https://github.com/TalAter/annyang/blob/master/docs/FAQ.md#what-languages-are-supported
+          annyang.setLanguage('en');
+
+          // Start listening. You can call this here, or attach this call to an event, button, etc.
+          annyang.start();
+        } else {
+            $(document).ready(function() {
+                $('#unsupported').fadeIn('fast');
+            });
+        }
+  });
+});
